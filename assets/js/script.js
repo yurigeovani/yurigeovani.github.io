f93e3f65ff0ff7a8603aac2b569b6058994db5ae
Yuri Geovani <yuri_geovani@hotmail.com>
2024-04-22 21:57:25 +0100

    Until contatcs and about

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,39 +66,40 @@
       agFlag = true;
     }
 
-    //script to send email and redirect to another page
-    function submitForm() {
-      // Get form data
-      const name = document.getElementById('name').value;
-      const email = document.getElementById('email').value;
-      const message = document.getElementById('message').value;
     
-      // Perform form submission (e.g., using fetch API to send data to Formspree)
-      /* method="post" action="https://formspree.io/f/mwkgarkd" */
-      fetch('https://formspree.io/f/mwkgarkd', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          name: name,
-          email: email,
-          message: message
-        })
-      })
-      .then(response => {
-        if (response.ok) {
-          // Redirect to another page upon successful submission
-          window.location.href = 'index.html'; // Change 'success.html' to your desired success page
-        } else {
-          throw new Error('Failed to submit form');
-        }
-      })
-      .catch(error => {
-        console.error('Form submission error:', error);
-        // Handle error (e.g., display error message to user)
-      });
-    }    
-
   });
+
 })(jQuery);
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  document.getElementById('contactForm').addEventListener('submit', function(event) {
+        event.preventDefault(); 
+
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const message = document.getElementById('message').value;
+      
+        fetch('https://formspree.io/f/mwkgarkd', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            name: name,
+            email: email,
+            message: message
+          })
+        })
+        .then(response => {
+          if (response.ok) {
+            window.location.href = "#email-sent";
+          } else {
+            throw new Error('Failed to submit form');
+          }
+        })
+        .catch(error => {
+          console.error('Form submission error:', error);
+        });
+    });
+});
