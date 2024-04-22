e382b07c3c72637a835716f3d987acbecd2c714a
Yuri Geovani <yuri_geovani@hotmail.com>
2024-04-22 16:43:53 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,104 @@
+(function ($) {
+  $(function () {
+
+
+    $(window).on('scroll', function () {
+      fnOnScroll();
+    });
+
+    $(window).on('resize', function () {
+      fnOnResize();
+    });
+
+
+    var agTimeline = $('.js-timeline'),
+      agTimelineLine = $('.js-timeline_line'),
+      agTimelineLineProgress = $('.js-timeline_line-progress'),
+      agTimelinePoint = $('.js-timeline-card_point-box'),
+      agTimelineItem = $('.js-timeline_item'),
+      agOuterHeight = $(window).outerHeight(),
+      agHeight = $(window).height(),
+      f = -1,
+      agFlag = false;
+
+    function fnOnScroll() {
+      agPosY = $(window).scrollTop();
+
+      fnUpdateFrame();
+    }
+
+    function fnOnResize() {
+      agPosY = $(window).scrollTop();
+      agHeight = $(window).height();
+
+      fnUpdateFrame();
+    }
+
+    function fnUpdateWindow() {
+      agFlag = false;
+
+      agTimelineLine.css({
+        top: agTimelineItem.first().find(agTimelinePoint).offset().top - agTimelineItem.first().offset().top,
+        bottom: agTimeline.offset().top + agTimeline.outerHeight() - agTimelineItem.last().find(agTimelinePoint).offset().top
+      });
+
+      f !== agPosY && (f = agPosY, agHeight, fnUpdateProgress());
+    }
+
+    function fnUpdateProgress() {
+      var agTop = agTimelineItem.last().find(agTimelinePoint).offset().top;
+
+      i = agTop + agPosY - $(window).scrollTop();
+      a = agTimelineLineProgress.offset().top + agPosY - $(window).scrollTop();
+      n = agPosY - a + agOuterHeight / 2;
+      i <= agPosY + agOuterHeight / 2 && (n = i - a);
+      agTimelineLineProgress.css({height: n + "px"});
+
+      agTimelineItem.each(function () {
+        var agTop = $(this).find(agTimelinePoint).offset().top;
+
+        (agTop + agPosY - $(window).scrollTop()) < agPosY + .5 * agOuterHeight ? $(this).addClass('js-ag-active') : $(this).removeClass('js-ag-active');
+      })
+    }
+
+    function fnUpdateFrame() {
+      agFlag || requestAnimationFrame(fnUpdateWindow);
+      agFlag = true;
+    }
+
+    //script to send email and redirect to another page
+    function submitForm() {
+      // Get form data
+      const name = document.getElementById('name').value;
+      const email = document.getElementById('email').value;
+      const message = document.getElementById('message').value;
+    
+      // Perform form submission (e.g., using fetch API to send data to Formspree)
+      /* method="post" action="https://formspree.io/f/mwkgarkd" */
+      fetch('https://formspree.io/f/mwkgarkd', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: name,
+          email: email,
+          message: message
+        })
+      })
+      .then(response => {
+        if (response.ok) {
+          // Redirect to another page upon successful submission
+          window.location.href = 'index.html'; // Change 'success.html' to your desired success page
+        } else {
+          throw new Error('Failed to submit form');
+        }
+      })
+      .catch(error => {
+        console.error('Form submission error:', error);
+        // Handle error (e.g., display error message to user)
+      });
+    }    
+
+  });
+})(jQuery);
---
+++ timeline-ie/README.md
@@ -0,0 +1,4 @@
+# Timeline IE
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/alvarotrigo/pen/yLzBJaN](https://codepen.io/alvarotrigo/pen/yLzBJaN).
+
